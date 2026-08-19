66b192e17b1e71bc77617e4df86090f6e7432500
NemanjaMiloicic <nemanjamiloicice24@gmail.com>
2026-08-19 10:54:05 +0200

    Added DatabaseModule with TypeORM connection, docker-compose for Postgres

---
+++ backend/src/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config : ConfigService) => ({
+                type: 'postgres',
+                host: config.get('DB_HOST'),
+                port: +config.get('DB_PORT'),
+                username: config.get('DB_USERNAME'),
+                password: config.get('DB_PASSWORD'),
+                database: config.get('DB_NAME'),
+                entities: [],
+                synchronize: true
+            }),
+        }),
+    ],
+})
+export class DatabaseModule {}
